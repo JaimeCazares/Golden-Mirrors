2b7144ca163d177e995f167b5a7f7059bf1984bb
Jaime Cazares <jaimecazares136@gmail.com>
2026-01-07 19:10:04 -0700

    p4

--- conexion.php
+++ conexion.php
@@ -1,17 +1,18 @@
 <?php
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $esLocal = in_array($_SERVER['HTTP_HOST'], ['localhost', '127.0.0.1']);
 
 if ($esLocal) {
+    // XAMPP
     $servername = "localhost";
     $username   = "root";
     $password   = "";
     $database   = "golden";
 } else {
-    $servername = "srv526.hstgr.io"; // ⚠️ esto lo vamos a verificar
+    // HOSTINGER
+    $servername = "localhost"; // 🔥 CLAVE
     $username   = "u717657264_golden";
     $password   = "Jaimecazares7.";
     $database   = "u717657264_golden";
