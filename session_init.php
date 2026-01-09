0beaf3fc120116bb431506c276b07657edf7309b
Jaime Cazares <jaimecazares136@gmail.com>
2026-01-09 05:01:19 -0700

    kjlsddsbjks

--- session_init.php
+++ session_init.php
@@ -2,7 +2,16 @@
 ini_set('session.cookie_path', '/');
 ini_set('session.use_only_cookies', 1);
 
-if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+/**
+ * SOLO activar cookies seguras en HTTPS (Hostinger)
+ * En local NO
+ */
+$https = (
+    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || $_SERVER['SERVER_PORT'] == 443
+);
+
+if ($https) {
     ini_set('session.cookie_secure', 1);
     ini_set('session.cookie_samesite', 'None');
 }
