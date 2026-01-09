e4550282befa61428c063d5ed0db004a00cbe417
Jaime Cazares <jaimecazares136@gmail.com>
2026-01-09 08:45:32 -0700

    d

--- ahorrovaleria/ahorro.php
+++ ahorrovaleria/ahorro.php
@@ -38,7 +38,8 @@ if ($_SESSION['rol'] !== 'novia') {
     <div class="lista" id="listaAhorro"></div>
   </div>
 
-  <<div id="cuponOverlay" class="cupon-overlay" style="display:none;">
+  <div id="cuponOverlay" class="cupon-overlay" style="display:none;">
+
     <div class="cupon">
       <h2>🎟️ CUPÓN <span id="folioCupon"></span></h2>
       <p>
@@ -48,10 +49,10 @@ if ($_SESSION['rol'] !== 'novia') {
       </p>
       <button id="cerrarCupon">Cerrar 💕</button>
     </div>
-    </div>
+  </div>
 
 
-    <script src="/ahorrovaleria/ahorro.js?v=3"></script>
+  <script src="/ahorrovaleria/ahorro.js?v=3"></script>
 </body>
 
 </html>
